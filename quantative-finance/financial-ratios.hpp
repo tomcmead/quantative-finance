506ae31150e88e342d52442cebeaa0b724a594fe
tom <tomcmead0@gmail.com>
2022-06-08 20:45:04 +0100

    feat: Financial ratios header template added

---
+++ quantative-finance/financial-ratios.hpp
@@ -0,0 +1,14 @@
+/*************************************************************************//**
+* @file  financial-ratios.hpp
+* @brief Function prototypes, nested includes and constants for finanical 
+*        ratios operations
+*****************************************************************************/
+#ifndef _FINANCIAL_RATIOS_H
+#define _FINANCIAL_RATIOS_H
+
+/*----------------------------------------------------------------------------
+  prototypes
+----------------------------------------------------------------------------*/
+
+
+#endif // _FINANCIAL_RATIOS_H
